fb2070c376099e5aa8dd053a9da35a5ebb2644fb
Edouard Outrebon <edoubn@gmail.com>
2024-01-24 22:22:52 +0100

    Parse Battle Output to get infos

--- randombattle.js
+++ randombattle.js
@@ -1,4 +1,5 @@
 const Sim = require('pokemon-showdown');
+const {Dex} = require('pokemon-showdown');
 stream = new Sim.BattleStream();
 
 
@@ -9,16 +10,60 @@ stream = new Sim.BattleStream();
             const requestString = output.slice(output.indexOf("|request|") + "|request|".length);
             teamState = JSON.parse(requestString);
             if(teamState.side.id == 'p1'){
-                console.log(teamState.side.pokemon[0]);
+                //console.log(teamState.side.pokemon[0]);
             }
         }
         if(output.includes("|t:|")){
-            const updateString = output.slice(output.indexOf("update") + "update".length);
-            if(output.includes("|turn|")){
-                const turnNumber = output.slice(output.indexOf("|turn|") + "|turn|".length);
-                console.log("Turn :"+ turnNumber);
+            const updateString = output.slice(output.indexOf("update") + "update".length).split('\n');
+            for (const line of updateString){
+                if(line.startsWith('|move|p1a')){
+                    console.log("Move P1: "+line.slice('|move|p1a:'.length))
+                }else if(line.startsWith('|move|p2a')){
+                    console.log("Move P2 :" + line.slice('|move|p2a:'.length))
+                }else if(line.startsWith('|-damage|p1a')){
+                    console.log("Dmg P1 :" + line)
+                }else if(line.startsWith('|-damage|p2a')){
+                    console.log("Dmg P2 :" + line)
+                }else if(line.startsWith('|switch|p1a')){
+                    console.log("Switch P1 :" + line)
+                }else if(line.startsWith('|switch|p2a')){
+                    console.log("Switch P2 :" + line)
+                }else if(line.startsWith('|-boost|p1a')){
+                    console.log("Boost P1 :" + line)
+                }else if(line.startsWith('|-boost|p2a')){
+                    console.log("Boost P2 :" + line)
+                }else if(line.startsWith('|-unboost|p1a')){
+                    console.log("Unboost P1 :" + line)
+                }else if(line.startsWith('|-unboost|p2a')){
+                    console.log("unboost P2 :" + line)
+                }else if(line.startsWith('|-heal|p1a')){
+                    console.log("Heal P1:" + line)
+                }else if(line.startsWith('|-heal|p2a')){
+                    console.log("Heal P2:" + line)
+                }else if(line.startsWith('|-status|p1a')){
+                    console.log("Status P1:" + line)
+                }else if(line.startsWith('|-status|p2a')){
+                    console.log("Status P2:" + line)
+                }else if(line.startsWith('|-ability|p1a')){
+                    console.log("Ability P1:" + line)
+                }else if(line.startsWith('|-ability|p2a')){
+                    console.log("Ability P2:" + line)
+                }else if(line.startsWith('|-end|p1a')){
+                    console.log("End P1:" + line)
+                }else if(line.startsWith('|-end|p2a')){
+                    console.log("End P2:" + line)
+                }else if(line.startsWith('|-enditem|p1a')){
+                    console.log("Enditem P1:" + line)
+                }else if(line.startsWith('|-enditem|p2a')){
+                    console.log("Enditem P2:" + line)
+                }else if(line.startsWith('|-fail|p1a')){
+                    console.log("Fail P1:" + line)
+                }else if(line.startsWith('|-fail|p2a')){
+                    console.log("Fail P2:" + line)
+                }
 
             }
+            
         }
 
 
@@ -34,3 +79,10 @@ stream.write(`>p2 move 2`)
 
 stream.write(`>p1 switch 6`)
 stream.write(`>p2 move 3`)
+
+
+
+/*console.log(Dex.moves.get('Tackle'));
+console.log(Dex.species.get('Tyranitar'));
+console.log(Dex.abilities.get('Unnerve'));
+console.log(Dex.items.get('Leftovers'));*/
