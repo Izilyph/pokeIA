17fb2ec973be1bbe24f533233d6e54d323e7c508
Edouard Outrebon <edoubn@gmail.com>
2024-02-01 15:47:17 +0100

    Pokemons parsing

--- randombattle.js
+++ randombattle.js
@@ -1,7 +1,47 @@
 const Sim = require('pokemon-showdown');
 const {Dex} = require('pokemon-showdown');
-stream = new Sim.BattleStream();
+const pokemon = require('pokemon-showdown/dist/sim/pokemon');
 
+const actions = {
+    move: "move",
+    damage: "-damage",
+    switch: "switch",
+    boost: "-boost",
+    unboost: "-unboost",
+    heal: "-heal",
+    status: "-status",
+    ability: "-ability",
+    end: "-end",
+    enditem: "-enditem",
+    fail: "-fail"
+};
+
+let gameStateP1 = {
+    "yourTeam":{
+        "active":[],
+        "pokemons":[]
+    },
+    "ennemyTeam":{
+        "active":[],
+        "pokemons":[]
+    },
+    "terrain":{},
+    "damageCalc":{},
+};
+let gameStateP2 = {
+    "yourTeam":{
+        "active":[],
+        "pokemons":[]
+    },
+    "ennemyTeam":{
+        "active":[],
+        "pokemons":[]
+    },
+    "terrain":{},
+    "damageCalc":{},
+};
+
+stream = new Sim.BattleStream();
 
 (async () => {
     for await (const output of stream) {
@@ -10,65 +50,83 @@ stream = new Sim.BattleStream();
             const requestString = output.slice(output.indexOf("|request|") + "|request|".length);
             teamState = JSON.parse(requestString);
             if(teamState.side.id == 'p1'){
-                //console.log(teamState.side.pokemon[0]);
+                parsePokemons(gameStateP1,gameStateP2,teamState);
+                //console.log(gameStateP2.ennemyTeam.pokemons);
+                //console.log(gameStateP1.yourTeam.pokemons[Object.keys(gameStateP1.yourTeam.pokemons)[0]].ability)
+            }else{
+                parsePokemons(gameStateP2,gameStateP1,teamState);
             }
+            gameStateP1.damageCalc = {
+
+            };
+
+            gameStateP2.damageCalc = {
+
+            };
         }
         if(output.includes("|t:|")){
             const updateString = output.slice(output.indexOf("update") + "update".length).split('\n');
-            for (const line of updateString){
-                if(line.startsWith('|move|p1a')){
-                    console.log("Move P1: "+line.slice('|move|p1a:'.length))
-                }else if(line.startsWith('|move|p2a')){
-                    console.log("Move P2 :" + line.slice('|move|p2a:'.length))
-                }else if(line.startsWith('|-damage|p1a')){
-                    console.log("Dmg P1 :" + line)
-                }else if(line.startsWith('|-damage|p2a')){
-                    console.log("Dmg P2 :" + line)
-                }else if(line.startsWith('|switch|p1a')){
-                    console.log("Switch P1 :" + line)
-                }else if(line.startsWith('|switch|p2a')){
-                    console.log("Switch P2 :" + line)
-                }else if(line.startsWith('|-boost|p1a')){
-                    console.log("Boost P1 :" + line)
-                }else if(line.startsWith('|-boost|p2a')){
-                    console.log("Boost P2 :" + line)
-                }else if(line.startsWith('|-unboost|p1a')){
-                    console.log("Unboost P1 :" + line)
-                }else if(line.startsWith('|-unboost|p2a')){
-                    console.log("unboost P2 :" + line)
-                }else if(line.startsWith('|-heal|p1a')){
-                    console.log("Heal P1:" + line)
-                }else if(line.startsWith('|-heal|p2a')){
-                    console.log("Heal P2:" + line)
-                }else if(line.startsWith('|-status|p1a')){
-                    console.log("Status P1:" + line)
-                }else if(line.startsWith('|-status|p2a')){
-                    console.log("Status P2:" + line)
-                }else if(line.startsWith('|-ability|p1a')){
-                    console.log("Ability P1:" + line)
-                }else if(line.startsWith('|-ability|p2a')){
-                    console.log("Ability P2:" + line)
-                }else if(line.startsWith('|-end|p1a')){
-                    console.log("End P1:" + line)
-                }else if(line.startsWith('|-end|p2a')){
-                    console.log("End P2:" + line)
-                }else if(line.startsWith('|-enditem|p1a')){
-                    console.log("Enditem P1:" + line)
-                }else if(line.startsWith('|-enditem|p2a')){
-                    console.log("Enditem P2:" + line)
-                }else if(line.startsWith('|-fail|p1a')){
-                    console.log("Fail P1:" + line)
-                }else if(line.startsWith('|-fail|p2a')){
-                    console.log("Fail P2:" + line)
+            for (let line of updateString) {
+                for (const action in actions) {
+                    const prefix = `|${actions[action]}|`;
+                        if (line.startsWith(prefix)) {
+                            const regex = /[:,|]/;
+                            line = line.slice(line.startsWith('|-') ? 2 : 1);
+                            const act = line.split(regex).map(str => str.slice(str.startsWith(' ') ? 1 : 0));
+                            //console.log(act);
+                            break;
+                        }
+                    }
                 }
+            }
+            
+        
+        if(output.includes("|error|")){
+            const errorString = output.slice(output.indexOf("|error|"));
+            //console.log(errorString);
+        }
+    }
+})();
 
+function parsePokemons(gameState1,gameState2,team){
+    team.side.pokemon.forEach(pokemon => {
+        const details = pokemon.details.split(',');
+        gameState1.yourTeam.pokemons[details[0]] ={
+            "lv":details[1].slice(2),
+            "stats":pokemon.stats,
+            "condition":pokemon.condition,
+            "moves":pokemon.moves.map(move => {
+                move = Dex.moves.get(move);
+                return {
+                    "name":move.name,
+                    "accuracy":move.accuracy,
+                    "basePower":move.basePower,
+                    "category":move.category,
+                    "pp":move.pp,
+                    "priority":move.priority,
+                    "type": move.type,
+                    "status":move.status,
+                    "secondary":move.secondary
+                };
+            }),
+            "ability":Dex.abilities.get(pokemon.ability),
+            "item":pokemon.item
+        };
+        if(pokemon.active){
+            gameState1.yourTeam.active = pokemon;
+            if(!gameState2.ennemyTeam.pokemons.hasOwnProperty(details[0])){
+                pokeDetails = Dex.species.get(details[0]);
+                gameStateP2.ennemyTeam.pokemons[details[0]] = {
+                    "types":pokeDetails.types,
+                    "abilities":pokeDetails.abilities,
+                    "estimatedStats":pokeDetails.baseStats
+                }
             }
             
         }
-
-
-    }
-})();
+        
+    });
+}
 
 stream.write(`>start {"formatid":"gen9randombattle"}`);
 stream.write(`>player p1 {"name":"Alice"}`);
@@ -81,8 +139,8 @@ stream.write(`>p1 switch 6`)
 stream.write(`>p2 move 3`)
 
 
-
-/*console.log(Dex.moves.get('Tackle'));
+//console.log(Dex.moves.get('Rain Dance'));
+/*
 console.log(Dex.species.get('Tyranitar'));
 console.log(Dex.abilities.get('Unnerve'));
 console.log(Dex.items.get('Leftovers'));*/
