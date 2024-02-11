5406f0d33d447dc4271f6c68b6c74053e1856300
Axel LANTA <lantaaxel@gmail.com>
2024-02-11 10:50:37 +0100

    Exploding for await push 2bis

--- websocket.js
+++ websocket.js
@@ -10,7 +10,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 
 
-
+let gameid=0
 wss.on('connection', function connection(ws) {
     console.log('Client connected');
 
@@ -24,7 +24,8 @@ wss.on('connection', function connection(ws) {
         waitingPlayer = null; // Reset waiting player
 
         // Start a new game with the paired players
-        startGame(player1, player2);
+        startGame(player1, player2, gameid, gameid * 2, gameid * 2 + 1);
+        gameid++;
     }
 
     ws.on('close', function close() {
